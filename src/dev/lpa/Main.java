2196f045f9fe57a473a36855839ac8b5e8806bd9
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-01 12:48:44 +0200

    add RecursiveSumTask using fork/join

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -6,6 +6,43 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.*;
 
+class RecursiveSumTask extends RecursiveTask<Long> {
+
+  private final long[] numbers;
+  private final int start;
+  private final int end;
+  private final int division;
+
+  public RecursiveSumTask(long[] numbers, int start, int end, int division) {
+    this.numbers = numbers;
+    this.start = start;
+    this.end = end;
+    this.division = division;
+  }
+
+  @Override
+  protected Long compute() {
+
+    if ((end - start) <= (numbers.length / division)) {
+      System.out.println(start + " : " + end);
+      long sum = 0;
+      for (int i = start; i < end; i++) {
+        sum += numbers[i];
+      }
+      return sum;
+    } else {
+      int mid = (start + end) / 2;
+      RecursiveSumTask leftTask =
+        new RecursiveSumTask(numbers, start, mid, division);
+      RecursiveSumTask rightTask =
+        new RecursiveSumTask(numbers, mid, end, division);
+      leftTask.fork();
+      rightTask.fork();
+      return leftTask.join() + rightTask.join();
+    }
+  }
+}
+
 public class Main {
 
   public static void main(String[] args) throws Exception {
@@ -50,6 +87,10 @@ public class Main {
     }
 
     System.out.println("Thread Pool Sum = " + taskSum);
+
+    RecursiveTask<Long> task = new RecursiveSumTask(numbers, 0, numbersLength, 2);
+    long forkJoinSum = threadPool.invoke(task);
+    System.out.println("RecursiveTask sum is " + forkJoinSum);
 
     threadPool.shutdown();
 
